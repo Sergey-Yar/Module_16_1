2f0ddcc718a6373d2fbf4f7e1161c826e59a601f
Сергей <ssergant@mail.ru>
2025-02-08 00:12:29 +0300

    Знакомство с FastAPI

---
+++ module_16_1.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI
+
+# Создаем экземпляр приложения FastAPI
+app = FastAPI()
+
+# Определение базового маршрута
+@app.get("/")
+async def root() -> dict:
+    return {"message": "Главная страница"}
+
+@app.get("/user/admin")
+async def admin() -> dict:
+    return {"message": "Вы вошли как администратор"}
+
+@app.get("/user/{user_id}")
+async def get_user(user_id) -> dict:
+    return {"message": f"Вы вошли как пользователь № {user_id}"}
+
+@app.get("/user")
+async def info_user(username: str, age: int) -> dict:
+    return {"message": f"Информация о пользователе. Имя: {username}, Возраст: {age}"}
